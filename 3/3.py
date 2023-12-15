df02938f012c5b7cfc49cbc89413339d4db54f90
Matt Harrison <matt.harrison91@gmail.com>
2023-12-15 09:53:34 +0000

    q3 complete

--- 3/3.py
+++ 3/3.py
@@ -1,15 +1,18 @@
+import re
+
 with open("3/input.txt", "r") as f:
     global data 
     data = f.read().splitlines()
 
 symbols = set()
+stars = {}
 
 for y, line in enumerate(data):
     for x, char in enumerate(line):
         if not char.isnumeric() and not char == ".":
-            for dx in range(-1,2,1):
-                for dy in range(-1,2,1):
-                    symbols.add((x + dx, y + dy))
+            symbols.add((x,y))
+            if char == "*":
+                stars[(x,y)]=[]
 
 total = 0
 number = 0
@@ -19,15 +22,46 @@ for y, line in enumerate(data):
     for x, char in enumerate(line):
         if char.isnumeric():
             number = (number*10) + int(char)
-            if (x,y) in symbols:
-                adjacent = True
+            if not adjacent:
+                for dx in range(-1,2,1):
+                    for dy in range(-1,2,1):
+                        if (x + dx,y + dy) in symbols:
+                            adjacent = True         
+            
         elif number != 0:
             if adjacent:
                 total += number
             number = 0
             adjacent = False
 
-print(total)
+print("Part 1",total)
+
+
+number = 0
+
+for r, row in enumerate(data):
+    for n in re.finditer(r'\d+', row):
+        around = {(c,r) for r in (r-1,r,r+1)
+                        for c in range(n.start()-1,n.end()+1)}
+        
+        for a in around & stars.keys():
+            stars[a].append(int(n.group()))
+
+total = 0
+
+for star in stars.values():
+
+    if len(star) == 2:
+        total += star[0] * star[1]
+
+print("Part 2",total)
+
+
+
+
+
+
+
 
 
 
